59f31e90e20809a49fd53070fdb38c3f1dbf08a7
Patrick Cando-Almeida <patrick.cando@infinityworks.com>
2023-02-24 15:03:56 +0000

    Update AllocationService to pass idempotency test

--- allocate.py
+++ allocate.py
@@ -1,14 +1,14 @@
 # Action --> Calculation --> Data
 class AllocationService:
-    def check_already_allocated(self):
-        pass
-
-def pre_allocate_check(order_line, batch) -> bool:
-    return batch.available_qty >= order_line.qty
-
-
-def allocate(order_line, batch, can_allocate):
-    if can_allocate:
-        batch.available_qty = batch.available_qty - order_line.qty
-    return batch.available_qty
-
+    def __init__(self):
+        self.allocations = {}
+    @staticmethod
+    def pre_allocate_check(order_line, batch):
+        return batch.available_qty >= order_line.qty
+    def check_already_allocated(self, order_line):
+        return order_line not in self.allocations
+    def allocate(self, order_line, batch, can_allocate, not_already_allocated):
+        if can_allocate and not_already_allocated:
+            batch.available_qty = batch.available_qty - order_line.qty
+            self.allocations[order_line] = 1
+        return batch.available_qty
