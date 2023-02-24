7581bc658c6ad0e7d1f5c280c6d5e837b5905855
Patrick Cando-Almeida <patrick.cando@infinityworks.com>
2023-02-24 15:07:19 +0000

    Formatting update

--- allocate.py
+++ allocate.py
@@ -1,12 +1,16 @@
 # Action --> Calculation --> Data
 class AllocationService:
+
     def __init__(self):
         self.allocations = {}
+
     @staticmethod
     def pre_allocate_check(order_line, batch):
         return batch.available_qty >= order_line.qty
+
     def check_already_allocated(self, order_line):
         return order_line not in self.allocations
+
     def allocate(self, order_line, batch, can_allocate, not_already_allocated):
         if can_allocate and not_already_allocated:
             batch.available_qty = batch.available_qty - order_line.qty
