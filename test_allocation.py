8103057db15f1bc62b9496267ac1ac6d4e496885
Daniel Lui <DanielLui@Daniels-iMac.local>
2023-02-24 11:49:28 +0000

    create allocation class

--- test_allocation.py
+++ test_allocation.py
@@ -3,33 +3,30 @@ from order_line import OrderLine
 from allocate import allocate, pre_allocate_check
 
 
-def test_pre_allocation_check_on_order_line_and_batch():
-    order_line = OrderLine(10)
-    batch = Batch(20)
+class TestAllocation:
 
-    assert pre_allocate_check(order_line, batch)
+    def test_pre_allocation_check_on_order_line_and_batch(self):
+        order_line = OrderLine(10)
+        batch = Batch(20)
 
+        assert pre_allocate_check(order_line, batch)
 
-def test_batch_available_quantity_decreases_after_successful_order_line_allocation():
-    order_line = OrderLine(10)
-    batch = Batch(20)
+    def test_batch_available_quantity_decreases_after_successful_order_line_allocation(self):
+        order_line = OrderLine(10)
+        batch = Batch(20)
 
-    assert batch.available_qty == 20
-    allocate(order_line, batch, can_allocate=True)
-    assert batch.available_qty == 10
+        assert batch.available_qty == 20
+        allocate(order_line, batch, can_allocate=True)
+        assert batch.available_qty == 10
 
+    def test_batch_available_quantity_is_less_than_order_line_quantity_at_allocation(self):
+        order_line = OrderLine(20)
+        batch = Batch(10)
 
-def test_batch_available_quantity_is_less_than_order_line_quantity_at_allocation():
-    order_line = OrderLine(20)
-    batch = Batch(10)
+    def test_allocations_are_idempotent(self):
+        order_line = OrderLine("RED-CHAIR", 10)
+        batch = Batch("RED-CHAIR", 20)
 
-    allocate(order_line, batch, pre_allocate_check(order_line, batch))
-    assert batch.available_qty == 10
-
-def test_allocations_are_idempotent():
-    order_line = OrderLine("RED-CHAIR",10)
-    batch = Batch("RED-CHAIR", 20)
-
-    allocate(order_line, batch, pre_allocate_check(order_line, batch))
-    allocate(order_line, batch, pre_allocate_check(order_line, batch))
-    assert batch.available_qty == 10
+        allocate(order_line, batch, pre_allocate_check(order_line, batch))
+        allocate(order_line, batch, pre_allocate_check(order_line, batch))
+        assert batch.available_qty == 10
