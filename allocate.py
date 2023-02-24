e70bbdc672a93ffa8d859aaa9e0df7cf625ad764
Patrick Cando-Almeida <patrick.cando@infinityworks.com>
2023-02-24 14:16:37 +0000

    Add AllocationService

--- allocate.py
+++ allocate.py
@@ -1,4 +1,7 @@
 # Action --> Calculation --> Data
+class AllocationService:
+    def check_already_allocated(self):
+        pass
 
 def pre_allocate_check(order_line, batch) -> bool:
     return batch.available_qty >= order_line.qty
