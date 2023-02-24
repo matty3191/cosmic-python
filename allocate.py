2ce2841e3a2216b5e58eedf90380f30c1802794e
Patrick Cando-Almeida <patrick.cando@infinityworks.com>
2023-02-24 10:42:17 +0000

    Create Allocation.py

---
+++ allocate.py
@@ -0,0 +1,10 @@
+# Action --> Calculation --> Data
+
+def pre_allocate_check(order_line, batch) -> bool:
+    return batch.available_qty >= order_line.qty
+
+
+def allocate(order_line, batch, can_allocate):
+    if can_allocate:
+        batch.available_qty = batch.available_qty - order_line.qty
+    return batch.available_qty
