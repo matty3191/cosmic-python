6dd7764fb2bdfba508ad2eb9a94655470474706e
Patrick Cando-Almeida <patrick.cando@infinityworks.com>
2023-02-24 15:03:21 +0000

    Update tests to use AllocationService

--- test_allocation.py
+++ test_allocation.py
@@ -1,27 +1,32 @@
 from batch import Batch
 from order_line import OrderLine
-from allocate import allocate, pre_allocate_check, AllocationService
+from allocate import AllocationService
+
 
 class TestAllocation:
 
     def test_pre_allocation_check_on_order_line_and_batch(self):
         order_line = OrderLine(sku="RED-CHAIR", qty=10)
         batch = Batch(sku="RED-CHAIR", available_qty=20)
-        assert pre_allocate_check(order_line, batch)
+
+        assert AllocationService.pre_allocate_check(order_line, batch)
 
     def test_batch_available_quantity_decreases_after_successful_order_line_allocation(self):
         order_line = OrderLine(sku="REC-CHAIR", qty=10)
         batch = Batch(sku="RED-CHAIR", available_qty=20)
+        allocation_service = AllocationService()
 
         assert batch.available_qty == 20
-        allocate(order_line, batch, can_allocate=True)
+        allocation_service.allocate(order_line, batch, can_allocate=True, not_already_allocated=True)
         assert batch.available_qty == 10
 
     def test_batch_available_quantity_is_less_than_order_line_quantity_at_allocation(self):
         order_line = OrderLine(sku="RED-CHAIR", qty=20)
         batch = Batch(sku="RED-CHAIR", available_qty=10)
+        allocation_service = AllocationService()
 
-        allocate(order_line, batch, pre_allocate_check(order_line, batch))
+        allocation_service.allocate(order_line, batch, AllocationService.pre_allocate_check(order_line, batch),
+                                    not_already_allocated=True)
         assert batch.available_qty == 10
 
     def test_allocations_are_idempotent(self):
@@ -30,7 +35,9 @@ class TestAllocation:
         batch = Batch(sku="RED-CHAIR", available_qty=20)
 
         assert batch.available_qty == 20
-        allocation_service.allocate(order_line, batch, pre_allocate_check(order_line, batch))
+        allocation_service.allocate(order_line, batch, AllocationService.pre_allocate_check(order_line, batch),
+                                    allocation_service.check_already_allocated(order_line))
         assert batch.available_qty == 10
-        allocation_service.allocate(order_line, batch, pre_allocate_check(order_line, batch))
+        allocation_service.allocate(order_line, batch, AllocationService.pre_allocate_check(order_line, batch),
+                                    allocation_service.check_already_allocated(order_line))
         assert batch.available_qty == 10
